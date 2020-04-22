6f81d3d0e172f6d609dfafbf033b0295e736af42
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-04-22 18:05:27 -0300

    criando estrutura banco de dados

--- backend/server/main.ts
+++ backend/server/main.ts
@@ -5,7 +5,6 @@ const express = require('express')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(express.static('public'));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- unit/estrtutura.dados/Endereco.ts
+++ unit/estrtutura.dados/Endereco.ts
@@ -1,9 +1,11 @@
+import { Cidade } from "./Cidade"
+
 export class Endereco {
   private rua: string;
   private bairro: string;
   private numero: string;
   private complemento: string;  
-
+  private cidade: Cidade
   constructor(
     rua,
     bairro,
